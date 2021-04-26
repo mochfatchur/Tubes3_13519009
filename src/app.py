f79bd961e7c4c1c1610da47a371c14f1402a27c5
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2021-04-26 12:58:47 +0700

    Chat is now functional

--- src/app.py
+++ src/app.py
@@ -3,17 +3,25 @@
 import os
 
 from flask import (
-    Flask, render_template, request
+    Flask, render_template, request, g, redirect
 )
+
+global chat_data
+chat_data = [("Halo", "Hai")]
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", message_data = chat_data[(-5 if len(chat_data) >= 5 else 0):])
     
 @app.route('/', methods=["POST"])
 def hello2():
-    return "You said \"{}\"".format(request.form["user-input"])
+    user_input = request.form["user-input"]
+    bot_response = "Hahahayyuk"
+    chat_data.append((user_input, bot_response))
+    return render_template("index.html", message_data = chat_data[(-5 if len(chat_data) >= 5 else 0):])
+    # return "You said \"{}\"".format(request.form["user-input"])
     
 app.run(debug=True)
+print(chat_data)
