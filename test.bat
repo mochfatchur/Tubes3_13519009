624ad454288b4e048ba64fc3f529cccc217a99d0
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2021-04-28 13:50:25 +0700

    Add GetSpesificTimeLeftTaskCommandTest.py to test batch

--- test.bat
+++ test.bat
@@ -3,8 +3,8 @@ cd src
 pytest -q DatabaseClassTest.py
 pytest -q GetDueTodayTaskCommandTest.py
 pytest -q AddTaskCommandTest.py
-pytest -q DeleteTaskTest.py
+pytest -q DeleteTaskCommandTest.py
 pytest -q GetAllTaskCommandTest.py
-pytest -q GetSpecificTimeLeftTaskTest.py
+pytest -q GetSpesificTimeLeftTaskCommandTest.py
 pytest -q ExtractorTest.py
 cd ..
